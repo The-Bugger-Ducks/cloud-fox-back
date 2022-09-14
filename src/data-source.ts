fbc35c4487aa1663bcdf835662c97c15b1103674
thhenrique <henriquethiago298@gmail.com>
2022-09-14 10:10:43 -0300

    added ssl property to data source connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,15 @@
-import 'dotenv/config' 
+import 'dotenv/config'
 
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 dotenv.config();
-
 
 export const AppDataSource = new DataSource({
   "type": "postgres",
   "host": process.env.DB_HOST || 'localhost',
   "port": parseInt(process.env.DB_PORT),
   "username": process.env.DB_USERNAME,
-  "password": process.env.DB_PASSWORD ,
+  "password": process.env.DB_PASSWORD,
   "database": process.env.DB_NAME,
   "entities": [
     "src/app/entities/*.ts"
@@ -21,4 +20,5 @@ export const AppDataSource = new DataSource({
   synchronize: false,
   logging: false,
   subscribers: [],
+  ssl: process.env.DB_SSL === 'true'
 });
